cd4e630e4945d4651eabd2fb579424cb6f276a67
Nicolas Auler <nicolasauler@usp.br>
2024-03-05 01:09:38 -0300

    Changing hyprland config a bit

--- hosts/notebook/home.nix
+++ hosts/notebook/home.nix
@@ -89,7 +89,7 @@
     enable = true;
 
     settings = {
-      monitor = ",preferred,auto,auto";
+      monitor = ",preferred,auto,1";
 
       exec-once = [
         "waybar"
@@ -165,20 +165,7 @@
       };
 
       animations = {
-        enabled = true;
-
-        # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-
-        animation = [
-          "windows, 1, 7, myBezier"
-          "windowsOut, 1, 7, default, popin 80%"
-          "border, 1, 10, default"
-          "borderangle, 1, 8, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
-        ];
+        enabled = false;
       };
 
       dwindle = {
@@ -208,8 +195,8 @@
       bind =
         [
           "$mainMod, return, exec, alacritty"
-          "$mainMod, C, killactive,"
-          "$mainMod, Q, exit,"
+          "$mainMod, Q, killactive,"
+          "$mainMod, E, exit,"
           "$mainMod, F, fullscreen,"
           "$mainMod SHIFT, F, togglefloating,"
           "$mainMod, D, exec, rofi -modi drun,run -show drun"
