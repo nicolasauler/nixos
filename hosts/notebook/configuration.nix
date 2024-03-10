9078d6240e8347e8146656cb5e1e8769c4bc511c
Nicolas Auler <nicolasauler@usp.br>
2024-03-09 22:44:01 -0300

    adding ld enable and cache

--- hosts/notebook/configuration.nix
+++ hosts/notebook/configuration.nix
@@ -171,4 +171,26 @@
     enable = true;
     cpuFreqGovernor = "performance";
   };
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
+
+  nix.settings = {
+    trusted-substituters = [
+      "https://cache.nixos.org/"
+    ];
+    substituters = [
+      "https://cuda-maintainers.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
 }
