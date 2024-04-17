174d141f8653f45470fe23ab164b5a34142cbc61
Nicolas Auler <nicolasauler@usp.br>
2024-04-17 16:08:56 -0300

    Added completion to nixvim

--- modules/programs/nixvim/plugins/cmp.nix
+++ modules/programs/nixvim/plugins/cmp.nix
@@ -7,10 +7,53 @@
       cmp-buffer.enable = true;
       cmp-path.enable = true;
       cmp-cmdline.enable = true;
-    };
 
-    plugins.cmp = {
-      enable = true;
+      lspkind = {
+        enable = true;
+
+        cmp = {
+          enable = true;
+          menu = {
+            nvim_lsp = "[LSP]";
+            nvim_lua = "[lua]";
+            path = "[path]";
+            luasnip = "[snip]";
+            buffer = "[buffer]";
+            # vim-dadbod-completion = "[dadbod]";
+          };
+        };
+      };
+
+      cmp = {
+        enable = true;
+        settings = {
+          snippet.expand = "luasnip";
+          # snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "luasnip"; }
+            { name = "nvim_lua"; }
+            { name = "path"; }
+            {
+              name = "buffer";
+              # Words from other open buffers can also be suggested.
+              option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+            }
+            #{ name = "vim-dadbod-completion"; }
+          ];
+          mapping = {
+            "<C-y>" = "cmp.mapping.confirm({ select = true })";
+            "<C-Space>" = "cmp.mapping.complete()";
+            #"<C-p>" = "cmp.mapping.select_prev_item(cmp_select)";
+            #"<C-n>" = "cmp.mapping.select_next_item(cmp_select)";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+        };
+      };
     };
   };
 }
