0eaaf862dafde7673eacfc65a275384c50647dc1
Nicolas Auler <nicolasauler@usp.br>
2024-07-16 21:40:17 -0300

    feat: adding nixvim flash functionality (treesitter and remote stuff) and keymaps

--- modules/programs/nixvim/plugins/flash.nix
+++ modules/programs/nixvim/plugins/flash.nix
@@ -1,5 +1,45 @@
 {
   programs.nixvim = {
-    plugins.flash.enable = true;
+    plugins.flash = {
+      enable = true;
+      settings = {
+        jump.autojump = true;
+        label.rainbow.enabled = true;
+        modes.char.jump_labels = true;
+      };
+    };
+
+    keymaps = [
+      {
+        key = "s";
+        action.__raw = "function() require('flash').jump() end";
+        mode = ["n" "x" "o"];
+        options.desc = "flash jump";
+      }
+      {
+        key = "S";
+        action.__raw = "function() require('flash').treesitter() end";
+        mode = ["n" "x" "o"];
+        options.desc = "flash treesitter";
+      }
+      {
+        key = "r";
+        action.__raw = "function() require('flash').remote() end";
+        mode = "o";
+        options.desc = "[r]emote flash";
+      }
+      {
+        key = "R";
+        action.__raw = "function() require('flash').treesitter_search() end";
+        mode = ["o" "x"];
+        options.desc = "flash treesitte[R] search";
+      }
+      {
+        key = "<c-s>";
+        action.__raw = "function() require('flash').toggle() end";
+        mode = "c";
+        options.desc = "toggle flash [s]earch";
+      }
+    ];
   };
 }
