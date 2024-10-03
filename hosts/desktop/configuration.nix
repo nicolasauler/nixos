1e2277f30753bf280c409b7438afa42aabd879e6
Nicolas Auler <nicolasauler@usp.br>
2024-10-03 13:49:53 -0300

    feat: adding postgres to desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -185,6 +185,32 @@
     opentabletdriver.enable = true;
   };
 
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql;
+
+    enableTCPIP = true;
+    settings.port = 6543;
+
+    # ensureDatabases = ["finapp"];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+
+      #type database DBuser origin-address auth-method
+      # ipv4
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host all       all     ::1/128        trust
+    '';
+
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE finapp WITH LOGIN PASSWORD 'finapp' CREATEDB;
+      CREATE DATABASE finapp;
+      GRANT ALL PRIVILEGES ON DATABASE finapp TO finapp;
+    '';
+  };
+
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     # Add any missing dynamic libraries for unpackaged programs
