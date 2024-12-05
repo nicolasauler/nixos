97cc255410bc8262eec25fb5d5e30c2e9939e809
Nicolas Auler <nicolasauler@usp.br>
2024-12-05 11:37:10 -0300

    feat: adding cachix enablings

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -307,6 +307,9 @@
   };
 
   nix.settings = {
+    trusted-users = [
+      "nic"
+    ];
     trusted-substituters = [
       "https://cache.nixos.org/"
     ];
@@ -314,11 +317,13 @@
       "https://cuda-maintainers.cachix.org"
       "https://hyprland.cachix.org"
       "https://devenv.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
