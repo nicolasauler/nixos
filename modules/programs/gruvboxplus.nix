3d2d75069a86e41819ed451baedaae358e314b5f
Nicolas Auler <nicolasauler@usp.br>
2025-02-26 09:30:40 -0300

    fix: gruvbox plus not working anymore, disabled theming

--- modules/programs/gruvboxplus.nix
+++ modules/programs/gruvboxplus.nix
@@ -1,12 +1,12 @@
 {pkgs}: let
-  link = "https://github.com/SylEleuth/gruvbox-plus-icon-pack/releases/download/v3.1/gruvbox-plus-icon-pack-3.1.zip";
+  link = "https://github.com/SylEleuth/gruvbox-plus-icon-pack/releases/download/v6.1.1/gruvbox-plus-icon-pack-6.1.1.zip";
 in
   pkgs.stdenv.mkDerivation {
     name = "gruvbox-plus";
 
     src = pkgs.fetchurl {
       url = link;
-      sha256 = "sha256-i/AzhYz/ACeXsG5j0kDVfvfA4TwxA3KZJTPwCO4BKmc=";
+      sha256 = "sha256-JsZwrqtyAF5+140U6/6PcKq4SM/4QyiR8da7yKh/CJI=";
     };
 
     dontUnpack = true;
