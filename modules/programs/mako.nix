c86926ce5858bfcc10dda64e832189135fc053ba
Nicolas Auler <nicolasauler@usp.br>
2025-05-09 10:08:51 -0300

    feat: mako config update

--- modules/programs/mako.nix
+++ modules/programs/mako.nix
@@ -1,15 +1,17 @@
 {config, ...}: {
   services.mako = {
     enable = true;
-    defaultTimeout = 4000;
-    backgroundColor = "#${config.colorScheme.palette.base01}";
-    borderColor = "#${config.colorScheme.palette.base0E}";
-    # backgroundColor = "#${config.colorScheme.palette.base01}";
-    # borderColor = "#${config.colorScheme.palette.base0E}";
-    borderRadius = 5;
-    borderSize = 2;
-    textColor = "#${config.colorScheme.palette.base04}";
-    # textColor = "#${config.colorScheme.palette.base04}";
-    layer = "overlay";
+    settings = {
+      default-timeout = 4000;
+      background-color = "#${config.colorScheme.palette.base01}";
+      border-color = "#${config.colorScheme.palette.base0E}";
+      # backgroundColor = "#${config.colorScheme.palette.base01}";
+      # borderColor = "#${config.colorScheme.palette.base0E}";
+      border-radius = 5;
+      border-size = 2;
+      text-color = "#${config.colorScheme.palette.base04}";
+      # textColor = "#${config.colorScheme.palette.base04}";
+      layer = "overlay";
+    };
   };
 }
