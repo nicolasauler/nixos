f3944cf17681d729d8f23bfe7cf5116a5adfdd14
Nicolas Auler <nickvarauler@gmail.com>
2026-07-16 20:54:27 -0300

    feat: starship jj module

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -1,4 +1,6 @@
-{...}: {
+{pkgs, ...}: {
+  home.packages = [pkgs.jj-starship];
+
   programs.starship = {
     enable = true;
     enableBashIntegration = true;
@@ -9,6 +11,22 @@
         truncation_length = 4;
       };
       gcloud = {
+        disabled = true;
+      };
+
+      # jj-starship replaces the built-in git modules: jj change/bookmarks in jj repos, branch/commit in plain git repos.
+      custom.jj = {
+        when = "jj-starship detect";
+        shell = ["jj-starship"];
+        format = "$output ";
+      };
+      git_branch = {
+        disabled = true;
+      };
+      git_commit = {
+        disabled = true;
+      };
+      git_status = {
         disabled = true;
       };
     };
